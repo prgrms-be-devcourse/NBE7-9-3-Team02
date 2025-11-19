1dc9340271663cf7e97d4ab3030aa6135d931515
Yejin <dpwls8972@gmail.com>
2025-11-19 23:30:19 +0900

    fix: applicationTest 마이그레이션

---
+++ backend/src/test/java/com/mysite/knitly/KnitlyApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.mysite.knitly
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class KnitlyApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
